c153110bbfc19f17f05e8fd5bedfe027642ae7b9
RD <klj40702@gmail.com>
2017-11-29 16:53:46 +0800

    add work link btn & add image

--- js/index.js
+++ js/index.js
@@ -159,19 +159,21 @@ let box_data = [
     'description': "<p>這是我大學的專題，是個能讓特殊族群出門旅遊能不用擔心去的地方有無無障礙空間、急救設備、客製化飲食等等，讓旅遊更安心、放心。</p><p>使用者可以依需求選擇美食或者住宿，還可篩選自身狀況與想要的距離，之後再排序來快速找出理想的店家。</p>"
   },
   {
-    "img": ["ntunhs-usedbook-01.png", 'ntunhs-usedbook-02.png'],
+    "img": ["ntunhs-usedbook-01.png", 'ntunhs-usedbook-02.png', 'ntunhs-usedbook-03.png', 'ntunhs-usedbook-04.png'],
     "title": "北護二手書交易平台",
     'description': "<p>這是一個提供給北護師生收購需用貨販售不要的二手書的平台。</p><p>大學時曾製作一個簡易版的二手書平台，之後持續這個概念，利用 Laravel 框架來完成這網頁，網頁加上 RWD，讓使用者不管是 Desptop 或是 Mobile 都能舒適的瀏覽內容。</p>"
   },
   {
     "img": ['Tic-Tac-Toe-01.png', 'Tic-Tac-Toe-02.png', 'Tic-Tac-Toe-03.png'],
     "title": "Tic Tac Toe",
-    'description': "<p>利用 JavaScript 製作出電腦版網頁玩的井字棋 (圈圈叉叉)。</p><p>玩家可以透過棋盤下方的圖示來得知該換哪個玩家，也能透過文字來得知當前狀況，玩得簡單又輕鬆。</p>"
+    'description': "<p>利用 JavaScript 製作出電腦版網頁玩的井字棋 (圈圈叉叉)。</p><p>玩家可以透過棋盤下方的圖示來得知該換哪個玩家，也能透過文字來得知當前狀況，玩得簡單又輕鬆。</p>",
+    "link" : 'works/Tic-Tac-Toe/index.html'
   },
   {
     "img": ["snake_game-01.png", 'snake_game-02.png', 'snake_game-03.png', 'snake_game-04.png'],
     "title": "貪食蛇",
-    'description': "<p>這是用 JavaScript 所做出的貪食蛇，玩家可以利用鍵盤來操控蛇的移動。</p><p>鍵盤的上下左右鍵為前後左右移動，或是用 F 來左轉，R 來右轉；P 為暫停；空白鍵為開始</p><p>當蛇吃到的食物越多，移動的速快越快，遊戲簡單好上手，等你來挑戰！</p>"
+    'description': "<p>這是用 JavaScript 所做出的貪食蛇，玩家可以利用鍵盤來操控蛇的移動。</p><p>鍵盤的上下左右鍵為前後左右移動，或是用 F 來左轉，R 來右轉；P 為暫停；空白鍵為開始</p><p>當蛇吃到的食物越多，移動的速快越快，遊戲簡單好上手，等你來挑戰！</p>",
+    'link' : 'works/snake_game/index.html'
   }
 ];
 
